ae1eecad5f18052b046b0ac9e878b661fb195e9a
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-12-27 12:02:53 -0800

    Update Bipartite Graph.java
    Link to the question:
    https://www.geeksforgeeks.org/problems/bipartite-graph/1

--- Graphs/Medium/Bipartite Graph.java
+++ Graphs/Medium/Bipartite Graph.java
@@ -1,5 +1,35 @@
 class Solution {
     
+    private boolean dfs(ArrayList<ArrayList<Integer>> adj, int[] visited, int src, int color){
+        
+        visited[src] = color;
+        
+        for(Integer i: adj.get(src)){
+            if(visited[i] == -1){
+                if(color == 0){
+                    color = 1;
+                }
+                else{
+                    color = 0;
+                }
+                return dfs(adj, visited, i, color);
+                // if(dfs(adj, visited, i, color)){
+                //     return true;
+                // }
+                // else{
+                //     return false;
+                // }
+            }
+            else{
+                if(visited[i] == color){
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+    
     private boolean bfs(ArrayList<ArrayList<Integer>> adj, int[] visited){
         
         Queue<Integer> queue = new LinkedList<>();
@@ -36,6 +66,10 @@ class Solution {
         int[] visited = new int[adj.size()];
         Arrays.fill(visited, -1);
         
-        return bfs(adj, visited);
+        visited[0] = 0;
+        
+        return dfs(adj, visited, 0, 0);
+        
+        // return bfs(adj, visited);
     }
 }
