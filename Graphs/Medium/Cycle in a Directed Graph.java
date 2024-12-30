bf7ec88d61d696b55d44a516ad2cebf923a56305
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-12-30 12:22:53 -0800

    Update Cycle in a Directed Graph.java
    Link to the question:
    https://www.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1

--- Graphs/Medium/Cycle in a Directed Graph.java
+++ Graphs/Medium/Cycle in a Directed Graph.java
@@ -1,5 +1,36 @@
 class Solution {
     
+    private boolean topoBfs(ArrayList<ArrayList<Integer>> adj, int[] inDegree){
+        
+        Queue<Integer> queue = new LinkedList<>();
+        int n = 0;
+        
+        for(int i = 0; i < inDegree.length; i++){
+            if(inDegree[i] == 0){
+                queue.add(i);
+            }
+        }
+        
+        while(!queue.isEmpty()){
+            int poll = queue.poll();
+            n++;
+            
+            for(Integer i: adj.get(poll)){
+                inDegree[i]--;
+                
+                if(inDegree[i] == 0){
+                    queue.add(i);
+                }
+            }
+        }
+        
+        if(n != inDegree.length){
+            return true;
+        }
+        
+        return false;
+    }
+    
     private boolean dfs(ArrayList<ArrayList<Integer>> adj, int[] visited, int[] pathVisited, int src){
         
         visited[src] = 1;
@@ -27,17 +58,32 @@ class Solution {
     public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
         // code here
         
-        int[] visited = new int[V];
-        int[] pathVisited = new int[V];
+        int[] inDegree = new int[V];
         
-        for(int i = 0; i < V; i++){
-            if(visited[i] == 0){
-                if(dfs(adj, visited, pathVisited, i)){
-                    return true;
-                }
+        for(List<Integer> list: adj){
+            for(Integer i: list){
+                inDegree[i]++;
             }
         }
         
-        return false;
+        return topoBfs(adj, inDegree);
+        
+        
+        
+        
+        
+        
+        // int[] visited = new int[V];
+        // int[] pathVisited = new int[V];
+        
+        // for(int i = 0; i < V; i++){
+        //     if(visited[i] == 0){
+        //         if(dfs(adj, visited, pathVisited, i)){
+        //             return true;
+        //         }
+        //     }
+        // }
+        
+        // return false;
     }
 }
