e0e6c38c3eb60730e0bbcbc3e69770ab8fe042b8
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-12-30 12:09:17 -0800

    Update Topological sort.java
    Link to the question:
    https://www.geeksforgeeks.org/problems/topological-sort/1

--- Graphs/Medium/Topological sort.java
+++ Graphs/Medium/Topological sort.java
@@ -1,5 +1,31 @@
 class Solution {
     
+    private static void topoBfs(ArrayList<ArrayList<Integer>> adj, int[] inDegree, ArrayList<Integer> ans){
+        
+        Queue<Integer> queue = new LinkedList<>();
+        
+        for(int i = 0; i < inDegree.length; i++){
+            if(inDegree[i] == 0){
+                queue.add(i);
+            }
+        }
+        
+        while(!queue.isEmpty()){
+            int poll = queue.poll();
+            ans.add(poll);
+            
+            for(Integer i: adj.get(poll)){
+                inDegree[i]--;
+                
+                if(inDegree[i] == 0){
+                    queue.add(i);
+                }
+            }
+        }
+        
+        return;
+    }
+    
     private static void dfs(ArrayList<ArrayList<Integer>> adj, int[] visited, Stack<Integer> stack, int src){
         
         visited[src] = 1;
@@ -18,20 +44,41 @@ class Solution {
     static ArrayList<Integer> topologicalSort(ArrayList<ArrayList<Integer>> adj) {
         // Your code here
         
+        int[] inDegree = new int[adj.size()];
         ArrayList<Integer> ans = new ArrayList<>();
-        int[] visited = new int[adj.size()];
-        Stack<Integer> stack = new Stack<>();
         
-        for(int i = 0; i < adj.size(); i++){
-            if(visited[i] == 0){
-                dfs(adj, visited, stack, i);
+        for(List<Integer> list: adj){
+            for(Integer i: list){
+                inDegree[i]++;
             }
         }
         
-        while(!stack.isEmpty()){
-            ans.add(stack.pop());
-        }
+        topoBfs(adj, inDegree, ans);
         
         return ans;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // ArrayList<Integer> ans = new ArrayList<>();
+        // int[] visited = new int[adj.size()];
+        // Stack<Integer> stack = new Stack<>();
+        
+        // for(int i = 0; i < adj.size(); i++){
+        //     if(visited[i] == 0){
+        //         dfs(adj, visited, stack, i);
+        //     }
+        // }
+        
+        // while(!stack.isEmpty()){
+        //     ans.add(stack.pop());
+        // }
+        
+        // return ans;
     }
 }
