797d4c78803d2231c127cb7850867183991588d4
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-01-06 20:10:29 -0800

    Update Eventual Safe States.java
    Link to the question:
    https://www.geeksforgeeks.org/problems/eventual-safe-states/1

--- Graphs/Medium/Eventual Safe States.java
+++ Graphs/Medium/Eventual Safe States.java
@@ -29,21 +29,66 @@ class Solution {
         // Your code here
         
         List<Integer> ans = new ArrayList<>();
-        int[] visited = new int[V];
-        int[] pathVisited = new int[V];
-        int[] check = new int[V];
+        List<List<Integer>> list = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        int[] inDegree = new int[V];
         
         for(int i = 0; i < V; i++){
-            if(visited[i] == 0){
-                dfs(adj, visited, pathVisited, i, check);
-            }
+            list.add(new ArrayList<>());
         }
+        
         for(int i = 0; i < V; i++){
-            if(check[i] == 1){
-                ans.add(i);
+            for(Integer j: adj.get(i)){
+                list.get(j).add(i);
+                inDegree[i]++;
             }
         }
+        
+        for(int i = 0; i < V; i++){
+            if(inDegree[i] == 0){
+                queue.add(i);
+            }
+        }
+        
+        while(!queue.isEmpty()){
+            
+            int poll = queue.poll();
+            ans.add(poll);
+            
+            for(Integer i: list.get(poll)){
+                inDegree[i]--;
+                if(inDegree[i] == 0){
+                    queue.add(i);
+                }
+            }
+        }
+        
+        Collections.sort(ans);
         
         return ans;
+        
+        
+        
+        
+        
+        
+        
+        // List<Integer> ans = new ArrayList<>();
+        // int[] visited = new int[V];
+        // int[] pathVisited = new int[V];
+        // int[] check = new int[V];
+        
+        // for(int i = 0; i < V; i++){
+        //     if(visited[i] == 0){
+        //         dfs(adj, visited, pathVisited, i, check);
+        //     }
+        // }
+        // for(int i = 0; i < V; i++){
+        //     if(check[i] == 1){
+        //         ans.add(i);
+        //     }
+        // }
+        
+        // return ans;
     }
 }
