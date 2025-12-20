10cc6769a6b4b10086cb87aa6be90a6b30b0e3c6
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-12-19 19:53:25 -0800

    Create Implement Lower Bound.java
    Link to the question:
    https://www.geeksforgeeks.org/problems/implement-lower-bound/1

---
+++ Binary Search/Implement Lower Bound.java
@@ -0,0 +1,25 @@
+class Solution {
+    int lowerBound(int[] arr, int target) {
+        // code here
+        
+        // Time Complexity : O(log n)
+        
+        // Space Complexity : O(1)
+        
+        
+        int low = 0, high = arr.length - 1;
+        
+        while(low <= high){
+            int mid = (low + high) / 2;
+            
+            if(arr[mid] >= target){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        
+        return low;
+    }
+}
