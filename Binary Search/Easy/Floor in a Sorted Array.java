a7ece849c37a831caf8c0476e6768cfa6170ea00
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-12-19 20:13:00 -0800

    Create Floor in a Sorted Array.java
    Link to the question:
    https://www.geeksforgeeks.org/problems/floor-in-a-sorted-array-1587115620/1

---
+++ Binary Search/Easy/Floor in a Sorted Array.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int findFloor(int[] arr, int x) {
+        // code here
+        
+        // Time Complexity : O(log n)
+        
+        // Space Complexity : O(1)
+        
+        
+        int low = 0, high = arr.length - 1;
+        
+        while(low <= high){
+            int mid = (low + high) / 2;
+            
+            if(arr[mid] > x){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        
+        if(high < 0){
+            return -1;
+        }
+        if(low >= arr.length){
+            return arr.length - 1;
+        }
+        
+        return high;
+    }
+}
